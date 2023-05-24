d5ecb448e5b0360cf09e2e089b9eb0d8cd91d5c3
Lucas Souza <107756390+developerlucassouza@users.noreply.github.com>
2023-05-24 17:18:23 -0300

    update

--- scripts/elementos_auto.js
+++ scripts/elementos_auto.js
@@ -2,32 +2,29 @@ var cabeça = document.querySelector('head')
 var corpo = document.querySelector('body')
 var cabeçalho = document.querySelector('header')
 var principal = document.querySelector('main')
-
+var menuburguer = document.querySelector('span#burguer')
 var itens = document.querySelector('div#itens')
 
-// Importar icons do Google
+// Icone de menu hamburguer
+//<span class="material-symbols-outlined" id="burguer">menu</span>
+
+// --- Importar icons do Google
 cabeça.innerHTML += '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />'
 
-// Criando cabeçalho
+// --- Criando cabeçalho
 cabeçalho.innerHTML += '<a href="#">Python</a> <a href="#">HTML</a> <a href="#">CSS</a> <a href="#">JavaScript</a>'
 
-
-
-//'<span class="material-symbols-outlined" id="burguer">menu</span> <a href="#">Python</a> <a href="#">HTML</a> <a href="#">CSS</a> <a href="#">JavaScript</a>'
-
-var menuburguer = document.querySelector('span#burguer')
-
-// Abrir e fechar menu hamburguer
-
+// --- Abrir e fechar menu hamburguer
+menuburguer.addEventListener('onclick', clickHamb)
 function clickHamb() {
-    if (itens.style.display == 'block') {
+    if (itens.style.display == 'inline-block') {
         itens.style.display = 'none'
     } else {
-        itens.style.display = 'block'
+        itens.style.display = 'inline-block'
     }
 }
-menuburguer.addEventListener('onclick', clickHamb)
 
+// --- Responsividade no menu hamburguer
 corpo.addEventListener('onresize', mudouTamanho)
 function mudouTamanho() {
     if (window.innerWidth >= 1000) {
--- scripts/html.js
+++ scripts/html.js
@@ -1,4 +1,116 @@
 var pages = document.querySelector('div#itens')
-pages.innerHTML += '<a href="">Home</a>'
-pages.innerHTML += '<a href="">&ltp&gt</a>'
-pages.innerHTML += '<a href="">&lth1&gt</a>'
+
+// Home
+pages.innerHTML += '<a href="">HTML Home</a>'
+
+// A
+pages.innerHTML += '<a href="                   ">HTML &lta&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltabbr&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltaddress&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltarticle&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltaside&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltaudio&gt</a>'
+
+// B
+pages.innerHTML += '<a href="                   ">HTML &ltb&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltblockquote&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltbody&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltbr&gt</a>'
+
+// C
+pages.innerHTML += '<a href="                   ">HTML &ltcaption&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltcode&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltcol&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltcolgroup&gt</a>'
+
+// D
+pages.innerHTML += '<a href="                   ">HTML &ltdatalist&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltdd&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltdel&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltdetails&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltdiv&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltdl&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltdt&gt</a>'
+
+// E
+pages.innerHTML += '<a href="                   ">HTML &ltem&gt</a>'
+
+// F
+pages.innerHTML += '<a href="                   ">HTML &ltfieldset&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltfooter&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltform&gt</a>'
+
+// H
+pages.innerHTML += '<a href="                   ">HTML &lth1&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lth2&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lth3&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lth4&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lth5&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lth6&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lthead&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltheader&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lthtml&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lthr&gt</a>' 
+
+// I
+pages.innerHTML += '<a href="                   ">HTML &lti&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltiframe&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltimg&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltinput&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltins&gt</a>'
+
+// L
+pages.innerHTML += '<a href="                   ">HTML &ltlabel&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltlegend&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltli&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltlink&gt</a>'
+
+// M
+pages.innerHTML += '<a href="                   ">HTML &ltmain&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltmark&gt</a>'
+
+// N
+pages.innerHTML += '<a href="                   ">HTML &ltnav&gt</a>'
+
+// O
+pages.innerHTML += '<a href="                   ">HTML &ltol&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltoptgroup&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltoption&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltoutput&gt</a>'
+
+// P
+pages.innerHTML += '<a href="                   ">HTML &ltp&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltpre&gt</a>'
+
+// Q
+pages.innerHTML += '<a href="                   ">HTML &ltq&gt</a>'
+
+// S
+pages.innerHTML += '<a href="                   ">HTML &ltsection&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltselect&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltsmall&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltspan&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltstrong&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltsub&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltsummary&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltsup&gt</a>'
+
+// T
+pages.innerHTML += '<a href="                   ">HTML &lttable&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lttbody&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lttd&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lttextarea&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lttfoot&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltth&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltthead&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lttitle&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &lttr&gt</a>'
+
+// U
+pages.innerHTML += '<a href="                   ">HTML &ltu&gt</a>'
+pages.innerHTML += '<a href="                   ">HTML &ltul&gt</a>'
+
+// V
+pages.innerHTML += '<a href="                   ">HTML &ltvideo&gt</a>'
+
+// pages.innerHTML += '<a href="">HTML &lt &gt</a>'
