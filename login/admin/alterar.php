20b8da7a0095b942fbc1026fda180bf604e2a27f
Lucas Souza <107756390+developerlucassouza@users.noreply.github.com>
2023-11-08 17:28:16 -0300

    update

--- login/admin/alterar.php
+++ login/admin/alterar.php
@@ -16,11 +16,29 @@ if (isset($_POST['submit'])) {
     $nivel = $_POST['nivel'];
 
     if (empty($nome) or empty($email)) {
-        echo '<script>alert("Preencha todos os campos");</script>';
+        echo '
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+        <script> 
+        $(document).ready(function() {
+            $("#preencha").modal();
+        });
+        </script>';
     } else {
         $altera = new Usuarios();
         $altera->alteraUsuario($_GET['id_usuario'], $nome, $email, $nivel);
-        echo '<script>alert("Usuário alterado com sucesso!");</script>';
+        echo '
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+        <script> 
+        $(document).ready(function() {
+            $("#alterado").modal();
+        });
+        </script>';
     }
 }
 
@@ -36,6 +54,60 @@ if (isset($_POST['submit'])) {
     }
 </style>
 
+
+
+<!-- Modal para quando alterar usuario -->
+<div class="modal fade" id="alterado" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-success">
+                <h5 class="modal-title" id="exampleModalLabel">Sucesso!</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-dark">
+                Usuário alterado com sucesso!
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <!-- <button type="button" class="btn btn-primary">Salvar mudanças</button> -->
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal para preencher os campos -->
+<div class="modal fade" id="preencha" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header bg-danger">
+        <h5 class="modal-title" id="exampleModalLabel">Aviso!</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-dark">
+        Preencha todos os campos!!!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <!-- <button type="button" class="btn btn-primary">Salvar mudanças</button> -->
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
 <main class="container mt-3">
     <h2 class="alert alert-primary">
         <i class="fa-solid fa-pen"></i> Alterar Usuário
@@ -43,20 +115,22 @@ if (isset($_POST['submit'])) {
 
     <form method="POST" class="p-3 bg-light text-dark">
         <label for="nome">Nome</label>
-        <input type="text" name="nome" id="nome" class="form-control" autofocus
-        value="<?php echo $usuario['nome']; ?>">
+        <input type="text" name="nome" id="nome" class="form-control" autofocus value="<?php echo $usuario['nome']; ?>">
 
         <label for="email">Email</label>
-        <input type="email" name="email" id="email" class="form-control"
-        value="<?php echo $usuario['email']; ?>">
+        <input type="email" name="email" id="email" class="form-control" value="<?php echo $usuario['email']; ?>">
 
         <label for="nivel">Nível</label>
         <select name="nivel" id="nivel" class="form-control">
-            <option value="1" <?php if ($usuario['nivel'] == 1) {echo 'selected';}?>>Comum</option>
-            <option value="0" <?php if ($usuario['nivel'] == 0) {echo 'selected';}?>>Administrador</option>
+            <option value="1" <?php if ($usuario['nivel'] == 1) {
+                echo 'selected';
+            } ?>>Comum</option>
+            <option value="0" <?php if ($usuario['nivel'] == 0) {
+                echo 'selected';
+            } ?>>Administrador</option>
         </select>
 
-        <button type="submit" name="submit" class="btn btn-primary mt-3"> 
+        <button type="submit" name="submit" class="btn btn-primary mt-3">
             <i class="fa-solid fa-pen"></i> Alterar
         </button>
 
--- login/admin/redefinir-senha.php
+++ login/admin/redefinir-senha.php
@@ -6,9 +6,44 @@ include_once '../../classes/Usuarios.class.php';
 if (isset($_GET['id_usuario'])) {
     $redefine = new Usuarios();
     $redefine->redefinirSenha($_GET['id_usuario']);
-    echo '<script>alert("Senha redefinida!");</script>';
+    echo '
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+        <script> 
+        $(document).ready(function() {
+            $("#redefinida").modal();
+        });
+        </script>';
+
     echo '<meta http-equiv="refresh" content="0;url=alterar.php?id_usuario='. $_GET['id_usuario'] . '">';
 } else {
     echo '<meta http-equiv="refresh" content="0;url=./">';
 }
+?>
+
+<!-- Modal -->
+<div class="modal fade" id="redefinida" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">Salvar mudanças</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+include_once '../../footer.php';
 ?>
